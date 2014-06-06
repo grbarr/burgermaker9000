f248461a454186a7ae2fdfe134e1030cb073a525
Kaitlin <smith.kaitlin@gmail.com>
2014-06-06 16:23:33 -0400

    Merges stuff.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -27,18 +27,33 @@ public class GameController : MonoBehaviour {
 		string[] ingredients = new string[9] {"lettuce", "tomato", "patty", "pickles", "cheese", "patty", "patty", "patty", "patty",};
 		int num_ingredients = Random.Range(1, 5);
 
-		burger.Add ("bottom_bun");
+		burger.Add ("bun_bottom");
 
 		for (int i = 0; i < num_ingredients; i++) {
 			string ingredient = ingredients[Random.Range(0, 4)];
-			Debug.Log("Adding " + ingredient + " to burger");
 			burger.Add(ingredient);
 		}
 
-		burger.Add ("top_bun");
-		Debug.Log("Buger looks like:");
-		foreach(string burg_ing in burger) {
-			Debug.Log(burg_ing);
+		burger.Add ("bun_top");
+//		Debug.Log("Buger looks like:");
+//		foreach(string burg_ing in burger) {
+//			Debug.Log(burg_ing);
+//		}
+
+		DisplayGeneratedBurger();
+	}
+
+	public void DisplayGeneratedBurger() {
+		var burger_object = GameObject.Find ("burger");
+
+		foreach (string burg_ing in burger) {
+			var texture = Resources.Load<Texture2D>("burger_parts/" + burg_ing);
+			Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+			Debug.Log(sprite);
+			GameObject go = new GameObject(burg_ing);
+			GameObject.DontDestroyOnLoad(go);
+			SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+			sr.sprite = sprite;
 		}
 	}
 }
