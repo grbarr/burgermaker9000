27020a8545996d9e681cf1b2bed26cf361aaea22
Float <float@Floats-MacBook-Pro.local>
2014-06-06 13:25:39 -0400

    Adding clock

---
+++ Assets/Scripts/ClockCount.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockCount : MonoBehaviour {
+
+	public bool isCounting;
+	public float counterTime;
+	private float currentTime;
+	GameObject clockHand;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = counterTime;
+		clockHand = GameObject.Find ("ClockHand");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//when the timer ends
+		if (currentTime < 0) {
+			isCounting = false;
+		}
+
+		//Update the timer
+		if (isCounting) {
+			currentTime -= Time.deltaTime;
+		}
+		else
+			currentTime = counterTime;
+
+		//rotate the hand of the clock correctly
+		float angle;
+		if ( counterTime == 0f )
+			angle = 0;
+		else
+			angle = - currentTime / counterTime * 360;
+		Debug.Log (angle);
+		clockHand.transform.rotation = Quaternion.Euler (0, 0, angle);
+
+	}
+}
---
+++ Assets/Scripts/PositionByCamera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionByCamera : MonoBehaviour {
+
+	public enum ScreenEdge {LEFT, RIGHT, TOP, BOTTOM, TOPRIGHT};
+	public ScreenEdge screenEdge;
+	public float yOffset;
+	public float xOffset;
+
+	// Use this for initialization
+	void Start () {
+		// 1
+		Vector3 newPosition = transform.position;
+		Camera camera = Camera.main;
+		
+		// 2
+		switch(screenEdge)
+		{
+			// 3
+		case ScreenEdge.RIGHT:
+			newPosition.x = camera.aspect * camera.orthographicSize + xOffset;
+			newPosition.y = yOffset;
+			break;
+			
+			// 4
+		case ScreenEdge.TOP:
+			newPosition.y = camera.orthographicSize + yOffset;
+			newPosition.x = xOffset;
+			break;
+
+		case ScreenEdge.TOPRIGHT:
+			newPosition.x = camera.aspect * camera.orthographicSize + xOffset;
+			newPosition.y = camera.orthographicSize + yOffset;
+			break;
+		}
+		// 5
+		transform.position = newPosition;
+		//GameObject thisObject = GameObject.Find ("Clock");
+		//Debug.Log (thisObject.GetComponent(PositionByCamera).xOffset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
