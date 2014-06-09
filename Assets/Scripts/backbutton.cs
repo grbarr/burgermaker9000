6807683e678f541edc82b558ecec6dfc1da1273e
grahambarr <graham@uken.com>
2014-06-09 15:41:40 -0400

    fix layout on highscore

---
+++ Assets/Scripts/backbutton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class backbutton : MonoBehaviour {
+	public AudioSource clickSound; 
+	
+	void OnMouseDown() {
+		// On Click, load the first level.
+		this.clickSound.Play();
+		Application.LoadLevel("menu_scene");
+	}
+}
