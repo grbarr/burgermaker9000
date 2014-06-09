193c33493ad330ea75254c06f3570c1a430c1292
grahambarr <graham@uken.com>
2014-06-09 14:17:28 -0400

    add credit list

---
+++ Assets/Scripts/creditscript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creditscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var go = GameObject.Find ("creditlist");
+		go.guiText.text = "Jen Costa, Tina Chan, Nick Van Vugt,\n Tanner Rogalsky, Rebecca Dreezer,\n Christine Brual, Kaitlin Smith, Haven Szostak,\n Graham Barr";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
