281ce6eb8331c07dc65a3237be7f8070e6d1277d
grahambarr <graham@uken.com>
2014-06-09 13:27:53 -0400

    add buttons to mainmenu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -7,26 +7,9 @@ using System.Collections;
 public class MenuScript : MonoBehaviour {
 	public AudioSource clickSound; 
 
-	void OnGUI() {
-		const int buttonWidth = 120;
-		const int buttonHeight = 40;
-
-		// Draw a button to start the game
-		if (
-			GUI.Button(
-			// Center in X, 2/3 of the height in Y
-			new Rect(
-			Screen.width / 2 - (buttonWidth / 2),
-			(5 * Screen.height / 6) - (buttonHeight / 2),
-			buttonWidth,
-			buttonHeight
-			),
-				"Get yo burg' on!"
-			)
-		) {
-			// On Click, load the first level.
-			this.clickSound.Play();
-			Application.LoadLevel("scene1");
-		}
+	void OnMouseDown() {
+		// On Click, load the first level.
+		this.clickSound.Play();
+		Application.LoadLevel("scene1");
 	}
 }
