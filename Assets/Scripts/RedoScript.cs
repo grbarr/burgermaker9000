d44a095771e0ab6b818be74f75a3bf02704b28d3
Kaitlin <smith.kaitlin@gmail.com>
2014-06-09 14:32:13 -0400

    Adds reset button puns.

--- Assets/Scripts/RedoScript.cs
+++ Assets/Scripts/RedoScript.cs
@@ -3,14 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class RedoScript : MonoBehaviour {
-	public AudioSource clickSound; 
-	public AudioSource resetSound1;
+	public AudioSource clickSound;
+
+	public AudioSource[] resetSounds;
+
+	public void playResetSound() {
+		var sound = resetSounds [Random.Range (0, resetSounds.Length)];
+		sound.Play();
+	}
 	
 	void OnMouseDown() {
 		GameController gameControl = GameController.Instance;
 		gameControl.ClearPlayerBurger ();
 
 		this.clickSound.Play ();
-		this.resetSound1.Play ();
+		playResetSound();
 	}
 }
