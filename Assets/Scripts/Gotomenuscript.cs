b1108c78d3b68061eaf6cd5b58da8c1755d0b6cc
grahambarr <graham@uken.com>
2014-06-09 13:57:56 -0400

    add credits scene

---
+++ Assets/Scripts/Gotomenuscript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class Gotomenuscript : MonoBehaviour {
+	public AudioSource clickSound; 
+
+	void OnMouseDown() {
+		// On Click, load the first level.
+		this.clickSound.Play();
+		Application.LoadLevel("menu_scene");
+	}
+}
---
+++ Assets/Scripts/gotocreditscript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class gotocreditscript : MonoBehaviour {
+	public AudioSource clickSound; 
+	
+	void OnMouseDown() {
+		// On Click, load the first level.
+		this.clickSound.Play();
+		Application.LoadLevel("credits_scene");
+	}
+}
