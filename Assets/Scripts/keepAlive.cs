7cbf6492a86a345ec39f6b7eb06153843de18835
Kaitlin <smith.kaitlin@gmail.com>
2014-06-09 15:37:19 -0400

    Prevent music from playing multiple times.

--- Assets/Scripts/keepAlive.cs
+++ Assets/Scripts/keepAlive.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class keepAlive : MonoBehaviour {
 
+	public static bool started = false;
+
 	// Use this for initialization
 	void Start () {
+		if (keepAlive.started) {
+			GameObject.Destroy(this.gameObject);
+			return;
+		}
 		GameObject.DontDestroyOnLoad (this.gameObject);
+		keepAlive.started = true;
 	}
 }
