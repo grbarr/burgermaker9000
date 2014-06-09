8b71f85fa6c1b407139e3dd700cdc851e5f26e96
grahambarr <graham@uken.com>
2014-06-09 13:10:15 -0400

    adding redo button

---
+++ Assets/Scripts/RedoScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RedoScript : MonoBehaviour {
+	public AudioSource clickSound; 
+	
+	void OnMouseDown() {
+		GameController gameControl = GameController.Instance;
+		gameControl.ClearPlayerBurger ();
+	}
+}
